1d8d3ac85727daf658c722e312af97a5805dd6d3
FlyFayer-byte <krupa.mykhailo@icloud.com>
2025-11-01 23:23:58 +0100

    modified:   index.html 	modified:   src/components/App.tsx 	new file:   src/components/ClickCounter.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,53 @@
 // src/components/App.tsx
+import { useState } from "react";
 
 import Product from "./Product";
 import Alert from "./Alert";
 import Button from "./Button";
 import UserMenu from "./UserMenu";
+import ClickCounter from "./ClickCounter";
+
+interface Values {
+  x: number;
+  y: number;
+};
 
 export default function App() {
+
+  const [values, setValues] = useState<Values>({ x: 0, y: 0 });
+  const updateX = () => { 
+    setValues({
+      ...values,
+      x: values.x + 1,
+    });
+  };
+  const updateY = () => { 
+    setValues({
+      ...values,
+      y: values.y + 1,
+    });
+  };
+
+  const [clicks, setClick] = useState(0);
+  const handleClick = () => {
+    setClick(clicks + 1);
+  };
+
+
+  const [count, setCount] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMessage = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const updateValue = (key: keyof Values) => { 
+    setValues({
+      ...values,
+      [key]: values[key] + 1,
+    });
+  }
+
   return (
     <>
       <h1>Best selling</h1>
@@ -27,6 +69,25 @@ export default function App() {
       <Button variant="secondary" text="Follow"/>
 
       <UserMenu name="Guest" />
+
+      <button onClick={handleClick}>Number of clicks: { clicks }</button>
+
+      <ClickCounter value={clicks} onUpdate={handleClick} />
+      <ClickCounter value={clicks} onUpdate={handleClick} />
+
+      <button onClick={handleClick}>Clicked: {count}</button>
+      <button onClick={toggleMessage}>{isOpen ? "Hide massage" : "Show message"}</button>
+      
+      {isOpen && <p>🎉 Surprise! You toggled me.</p>}
+
+      <div>
+        <p>x: {values.x}, y: {values.y}</p>
+        <button onClick={updateX}>Update x</button>
+        <button onClick={updateY}>Update y</button>
+      </div>
+
+      <button onClick={() => updateValue("x")}>Update x</button>
+      <button onClick={() => updateValue("y")}>Update y</button>
     </>
   );
 }
---
+++ src/components/ClickCounter.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+
+interface ClickCounterProps { 
+    value: number;
+    onUpdate: () => void;
+};
+
+// export default function ClickCounter() {
+//   const [clicks, setClicks] = useState(0);
+
+//   const handleClick = () => {
+//     setClicks(clicks + 1);
+//   };
+
+//   return <button onClick={handleClick}>Clicked: {clicks}</button>;
+// }
+
+export default function ClickCounter({value, onUpdate}: ClickCounterProps) {
+//   const [clicks, setClicks] = useState(0);
+
+//   const handleClick = () => {
+//     setClicks(clicks + 1);
+//   };
+
+  return <button onClick={onUpdate}>Clicked: {value}</button>;
+}
